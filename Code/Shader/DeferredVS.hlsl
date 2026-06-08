b04f16ad720408b435efe40a2a09d7c5622147d7
YuukiOtokawa <ths30871@ths.hal.ac.jp>
2026-06-08 09:56:37 +0900

    fix : DeferredVS.hlslで画面が映らない問題を修正
    頂点計算いらないみたい

--- Code/Shader/DeferredVS.hlsl
+++ Code/Shader/DeferredVS.hlsl
@@ -4,17 +4,10 @@ PS_INPUT main(VS_INPUT input)
 {
     PS_INPUT output;
 
-    float4 localPos = float4(input.Position, 1.0f);
+    output.Position = float4(input.Position.xy, 0.0f, 1.0f);
 
-    // 行列は「position * World * View * Projection」
-    float4 worldPos = mul(localPos, World);
-    float4 viewPos = mul(worldPos, View);
-    output.Position = mul(viewPos, Projection);
-
-    float4 localN = float4(input.Normal, 0.0f);
-    output.Normal = mul(localN, World);
-
-    output.WorldPosition = worldPos;
+    output.WorldPosition = output.Position;
+    output.Normal = float4(0.0f, 0.0f, 1.0f, 0.0f);
     output.TexCoord = input.TexCoord;
     output.Color = input.Color;
     return output;
