3416db21999bb8f52f5cea8b9f9ce7dd191983e6
YuukiOtokawa <ths30871@ths.hal.ac.jp>
2026-06-08 09:54:44 +0900

    feat : EditorCameraの追加
    EditorCameraを追加し、画面表示するカメラビューを2つに変更

--- GameManager.cpp
+++ GameManager.cpp
@@ -48,7 +48,7 @@ namespace EngineManager {
 	EngineCore::General::Camera* GameManager::FindGameCamera() {
 		auto& objects = _ObjectManager->GetObjects();
 		for (const auto& entry : objects) {
-			if (entry.object && entry.object->GetName() != "EditorCamera") {
+			if (entry.object && entry.object->IsActive() && entry.object->GetName() != "EditorCamera") {
 				auto camera = entry.object->GetComponent<EngineCore::General::Camera>();
 				if (camera) {
 					return camera;
@@ -61,7 +61,7 @@ namespace EngineManager {
 	EngineCore::General::Camera* GameManager::FindEditorCamera() {
 		auto& objects = _ObjectManager->GetObjects();
 		for (const auto& entry : objects) {
-			if (entry.object && entry.object->GetName() == "EditorCamera") {
+			if (entry.object && entry.object->IsActive() && entry.object->GetName() == "EditorCamera") {
 				auto camera = entry.object->GetComponent<EngineCore::General::Camera>();
 				if (camera) {
 					return camera;
@@ -79,21 +79,33 @@ namespace EngineManager {
 		auto gameCamera = FindGameCamera();
 		if (gameCamera) {
 			EngineCore::General::Camera::SetActiveCamera(gameCamera);
+			m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::GAME_VIEW);
+			m_RenderManger.DrawBegin();
+			_ObjectManager->DrawObjects();
+			m_RenderManger.DrawEnd();
 		}
-		m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::GAME_VIEW);
-		m_RenderManger.DrawBegin();
-		_ObjectManager->DrawObjects();
-		m_RenderManger.DrawEnd();
+		else {
+			// Clear Game View to black if no active game camera exists
+			m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::GAME_VIEW);
+			m_RenderManger.DrawBegin();
+			m_RenderManger.DrawEnd();
+		}
 
 		// --- 2. Scene View Render Pass ---
 		auto editorCamera = FindEditorCamera();
 		if (editorCamera) {
 			EngineCore::General::Camera::SetActiveCamera(editorCamera);
+			m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::SCENE_VIEW);
+			m_RenderManger.DrawBegin();
+			_ObjectManager->DrawObjects();
+			m_RenderManger.DrawEnd();
 		}
-		m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::SCENE_VIEW);
-		m_RenderManger.DrawBegin();
-		_ObjectManager->DrawObjects();
-		m_RenderManger.DrawEnd();
+		else {
+			// Clear Scene View to black if no active editor camera exists
+			m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::SCENE_VIEW);
+			m_RenderManger.DrawBegin();
+			m_RenderManger.DrawEnd();
+		}
 
 		// --- 3. ImGui Pass (To Back Buffer) ---
 		m_RenderManger.SetCurrentTarget(Render::RenderManager::RENDER_TARGET_TYPE::BACK_BUFFER);
